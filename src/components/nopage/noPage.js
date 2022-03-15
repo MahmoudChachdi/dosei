498b851df0d8c7ffd8ed7bbd4e61f5a70906b4f5
mamo-1 <mahmoud.chachdi@gmail.com>
2022-03-15 15:52:10 +0000

    nopage

--- src/components/nopage/noPage.js
+++ src/components/nopage/noPage.js
@@ -12,7 +12,7 @@ const NoPage = () => {
       <h1 class="f1 f-headline-l code mb3 fw9 dib tracked-tight ">404</h1>
       <h2 class="tc  f1-l fw1">Sorry, we can't find the page you are looking for.</h2>
   </header>
-  <p class="fw1 i  tc mt4 mt5-l f4 f3-l">Are you looking for one of these?</p>
+  
   <ul class="list tc pl0 w-100 mt5">
 <div className='center' >
        <Link to="home" >
